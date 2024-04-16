3498ab52b9b0c46f6726b989afd06ddb27e52b67
Jenna Jones <jljones121@gmail.com>
2024-04-16 21:54:41 +0000

    back to original main.c

--- main.c
+++ main.c
@@ -1,36 +1,31 @@
 #include "monty.h"
 
+/**
+ * main - Entry point of the Monty interpreter
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: Always 0
+ */
+
 int main(int argc, char *argv[])
 {
 	FILE *file;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	unsigned int line_number = 0;
-	stack_t *stack = NULL;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
-	}
-
-	while ((read = getline(&line, &len, file)) != -1)
-	{
-		line_number++;
-		process_line(line, line_number, &stack);
+		exit(EXIT_FAILURE);
 	}
 
 	fclose(file);
-	if (line)
-		free(line);
 
-	return (EXIT_SUCCESS);
+	return (0);
 }
