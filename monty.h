71ceb8ec3f9cca9551afc1b9618533047f9088b9
Jenna Jones <jljones121@gmail.com>
2024-04-16 20:53:49 +0000

    push.c

--- monty.h
+++ monty.h
@@ -20,6 +20,7 @@ typedef struct stack_s
         struct stack_s *prev;
         struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -34,4 +35,8 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-#endif
+/* Function prototypes */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
---
+++ push.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * push - Pushes an element onto the stack
+ * @stack: Double pointer to the head of the stack
+ * @line_number: Line number in the Monty file
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	printf("Push opcode called\n");
+}
